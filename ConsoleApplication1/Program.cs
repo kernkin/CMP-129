65d119f2c0f018e22a4b93095632d1f86a31f88e
Jordan Saul <saul.jordan@student.ccm.edu>
2013-09-05 20:11:40 -0400

    First C# assignment
    Signed-off-by: Jordan Saul <saul.jordan@student.ccm.edu>

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //output one 1-100 every tenth is a different color : output two even is red, odd is blue
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Cyan on white");
+            Console.WriteLine("testing.");
+            Console.ResetColor();          
+            int 
+         }
+            {
+
+            }
+            
+    }
+}
