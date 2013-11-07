8de4fc54612c948b7f17e384a57c9188a73f83bd
Jordan <saul.jordan@student.ccm.edu>
2013-11-07 12:43:57 -0500

    interface

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+interface myInterface
+{
+    void Read();
+}
+
+class TestInterface : myInterface
+{
+    public void Read()
+    {
+        Console.WriteLine("Fluffy");
+    }
+}
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main()
+        {
+            myInterface Interfaces = new TestInterface();
+            Interfaces.Read();
+        }
+    }
+}
