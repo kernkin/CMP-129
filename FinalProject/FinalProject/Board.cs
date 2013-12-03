12aa735965de7b0342475fcc4df07cfb11274323
Jordan <saul.jordan@student.ccm.edu>
2013-12-03 13:19:37 -0500

    Final Project

---
+++ FinalProject/FinalProject/Board.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace FinalProject
+{
+    class Board
+    {
+        private Rectangle[,] slots = new Rectangle[3, 3];
+        private Holder[,] holders = new Holder[3, 3];
+
+        public const int X = 0;
+        public const int O = 1;
+        public const int B = 2;
+
+        public void initBoard()
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    slots[x, y] = new Rectangle(x * 167, y * 167, 167, 167);
+                    holders[x, y] = new Holder();
+                    holders[x, y].setValue(B);
+                    holders[x, y].setLocation(new Point(x, y));
+                }
+            }
+        }
+    }
+    class Holder
+    {
+        private Point location;
+        private int value = Board.B;
+
+        public void setLocation(Point p)
+        {
+            location = p;
+        }
+        public Point getLocation()
+        {
+            return location;
+        }
+        public void setValue(int i)
+        {
+            value = i;
+        }
+        public int getValue()
+        {
+            return value;
+        }
+    }
+}
---
+++ FinalProject/FinalProject/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class Form1 : Form
+    {
+        GFX engine;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics toPass = panel1.CreateGraphics();
+            engine = new GFX(toPass);
+
+            Board theBoard = new Board();
+            theBoard.initBoard();
+        }
+    }
+}
